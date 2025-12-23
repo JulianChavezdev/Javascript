42027040d5e92a673e55927155b3fc590973a710
JulianChavezDev <julicgavez1@gmail.com>
2025-12-23 14:14:03 +0100

    adding new page

---
+++ for_of.js
@@ -0,0 +1,13 @@
+/*
+for of, arrays, Strings [algo]
+
+for(variable of objeto) {
+    bloque de codigo a ejecutar
+}
+*/
+
+
+let canasta = ['manzana', 'banana', 'pera', 'uva', 'mango'];
+for (fruta of canasta){
+    console.log(fruta)
+}
