cd3249f975504d16028e37c7e22239fb3b04f2a0
JulianChavezDev <julicgavez1@gmail.com>
2025-12-23 13:34:29 +0100

    condicionales

---
+++ condicionales.js
@@ -0,0 +1,17 @@
+//if
+let edad = 18;
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad")
+}
+
+//if else if else
+let hora = 10;
+if (hora < 12) {
+    console.log("Buenos dias");
+} else if (hora < 18) {
+    console.log("Buenas tardes");
+} else {
+    console.log("Buenas noches");
+}
---
+++ operadoreslogicos.js
@@ -0,0 +1,13 @@
+//operadores logicos 
+
+// && AND
+// || OR
+// ! NOT
+
+const a = 10;
+const b = 20;
+const c = "10";
+
+console.log(a == b && a === c); //false
+console.log(a != b || a === c); //true
+console.log(!(a ===c)); //true
