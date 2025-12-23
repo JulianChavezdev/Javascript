2410d576c96882bbe52b14d874343e46ef46955c
JulianChavezDev <julicgavez1@gmail.com>
2025-12-23 13:28:44 +0100

    adding operadoreslogicos

---
+++ operadores_de_comparacion.js
@@ -0,0 +1,23 @@
+//operadores
+
+// == igualdad
+// === igualdad estricta
+// != desigualdad
+// !== desigualdad estricta
+// > mayor que
+// < menor que
+// >= mayor o igual que
+// <= menor o igual que
+
+const a = 10;
+const b = 20;
+const c = "10";
+
+console.log(a == c); //true
+console.log(a === c); //false
+console.log(a != b); //true
+console.log(a !== c); //true
+console.log(a > b); //false
+console.log(a < b); //true
+console.log(a >= c); //true
+console.log(a <= b); //true
