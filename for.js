20a4b2aee633cdec9188925c9bc7ddc097c086d9
JulianChavezDev <julicgavez1@gmail.com>
2025-12-23 14:02:33 +0100

    doing myself

--- for.js
+++ for.js
@@ -8,3 +8,10 @@ let list = ['eat', 'sleep', 'code', 'repeat'];
 for (let i = 0; i < list.length; i++) {
     console.log(list[i]);
 }
+
+
+let dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+
+for (let i = 0; i < dias.length; i++) {
+    console.log('Hoy es ' + dias[i]);
+}
