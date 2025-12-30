8191392d4de9023bb765a987e675eded552c6e95
JulianChavezDev <julicgavez1@gmail.com>
2025-12-30 13:36:46 +0100

    ap

---
+++ find_findIndex.js
@@ -0,0 +1,17 @@
+//find
+
+const multipleof5 = [5,10,15,29]
+
+const firstNumberGreaterThan10 = multipleof5.find(number => number >10)
+console.log(multipleof5)
+console.log(firstNumberGreaterThan10)
+
+
+//findIndex
+
+const randomnumber =[6,14,27,56,40]
+
+const indexNumber = randomnumber.findIndex(number => number > 50)
+
+console.log(randomnumber)
+console.log(indexNumber)
